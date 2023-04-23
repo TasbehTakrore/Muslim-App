348bdc8e8544369afa07326dc7df59ca1740f96b
AyahShraim <ayahshraim558@gmail.com>
2023-04-23 18:31:26 +0300

    pray

--- alquramcommunity_frontend/lib/view/screen/qibla.dart
+++ alquramcommunity_frontend/lib/view/screen/qibla.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../controller/prayscreen_controller.dart';
+
 import '../../controller/qiblascreen_controller.dart';
 import '../../core/constant/color.dart';
 import '../../core/constant/routes.dart';
-import '../widget/home/customcardhome.dart';
-import '../widget/prayingtimes/completedPray_dialog.dart';
-import '../widget/prayingtimes/location_dialog.dart';
-import '../widget/prayingtimes/prayingtopCard.dart';
-import '../widget/prayingtimes/prayslist.dart';
+
 
 class QiblaScreen extends StatelessWidget {
   const QiblaScreen({super.key});
